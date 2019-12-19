eb6ce3e7b2b1d4c12949592f5426444d0b828b8d
yhong4 <yhong.it@gmail.com>
2019-12-20 09:57:22 +1100

    test

--- api/handler.py
+++ api/handler.py
@@ -3,7 +3,7 @@ import json
 
 def hello(event, context):
     body = {
-        "message": "Go111 S111erverless v1.0! Your function executed successfully!",
+        "message": "Go111222 S111erverless v1.0! Your function executed successfully!",
         "input": event
     }
 
